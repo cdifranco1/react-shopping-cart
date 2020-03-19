97bd9ef9a7535fbf1d745dab1a271841cd1b842f
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-03-19 17:54:29 -0500

    completed mvp and removeItem stretch

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext';
 
-const Item = props => {
+const Item = (props) => {
+  console.log(props)
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
-
-
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={props.removeItem}>Remove from item</button>
 			</div>
 		</div>
 	);
